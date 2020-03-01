4bb51b3a48d4cddf9a5754950a4eae7c68707700
John D. Lewis <balain@users.noreply.github.com>
2020-02-29 21:31:41 -0500

    Updated to minimally working version

--- app.js
+++ app.js
@@ -1,18 +1,79 @@
-/*********************************
-/ From the Restify Quick Start
-/ http://restify.com/docs/home/
-/*********************************/
-var restify = require('restify');
+/*******************************
+ * app.js
+ *
+ * Created: 2/29/2020
+ * Author: John D. Lewis
+ * License: MIT
+ *******************************/
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+let auth = require('./auth.js');
+let config = require('./config.js');
+
+const port = config.port;
+
+var options = {
+  key: auth.key,
+  certificate: auth.cert
+};
+
+var restify = require('restify');
+var sqlite3 = require('sqlite3').verbose();
+
+// Restify setup
+var server = restify.createServer(options);
+server.use(restify.plugins.queryParser());
+
+// Sqlite setup
+const file = config.dbFilename;
+var db = new sqlite3.Database(file);
+
+// Restify auth
+server.use(function(req, res, next) {
+	if (req.query.admin && req.query.admin == auth.adminName) {
+		return next();
+	} else {
+		res.send(404);
+		return next(false);
+	}
+})
+
+// API methods
+server.get('/', function(req, res, next) { res.send(404); return next(false); });
+server.get('/lastname/:lastname', findLastName);
+server.get('/id/:id', findId);
+
+// API functions
+function findLastName(req, res, next) {
+	var sql = "SELECT ID, FullName, FirstName, LastName, WTitle FROM latest_data WHERE LastName = '" + req.params.lastname + "'";
+	db.all(sql, function(err, rows) {
+		if (err) {
+			res.send({errors: [err], meta: [], data: []});
+		} else {
+	    	res.send({errors: [], meta: [], data: rows});
+	    }
+	});	
+	next();
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+function findId(req, res, next) {
+	var sql = "SELECT * FROM latest_data WHERE ID = '" + req.params.id + "'";
+	db.all(sql, function(err, rows) {
+		if (err) {
+			res.send({errors: [err], meta: [], data: []});
+		} else {
+	    	res.send({errors: [], meta: [], data: rows});
+	    }
+	});	
+	next();
+}
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+process.on('SIGINT', function() {
+	console.log("Caught interrupt signal. Closing database connection...");
+	db.close();
+	console.log("...done");
+	process.exit();
+})
+
+server.listen(port, () => {
+  console.log("server starting on port : " + port)
 });
