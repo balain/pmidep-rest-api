0d39d9331170c1ef30c6e37a9c107904bbda2e99
John D. Lewis <balain@users.noreply.github.com>
2020-04-12 18:32:16 -0400

    CORS and authentication upgrades

--- app.js
+++ app.js
@@ -8,9 +8,9 @@
 
 let auth = require('./auth.js');
 let config = require('./config.js');
-
 let depfunc = require('./depfunc.js');
 
+const request = require('request');
 const port = config.port;
 
 var options = {
@@ -25,15 +25,48 @@ var sqlite3 = require('sqlite3').verbose();
 var server = restify.createServer(options);
 server.use(restify.plugins.queryParser());
 
+server.use(
+  function crossOrigin(req,res,next){
+    if (config.okOrigins.includes(req.headers.origin)) {
+      res.header("Access-Control-Allow-Origin", req.headers.origin);
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      console.log("Serving request to ", req.headers.origin);
+    }
+    return next();
+  }
+);
+
 // Sqlite setup
 const file = config.dbFilename;
-var db = new sqlite3.Database(file, sqlite3.OPEN_READONLY);
+var db = new sqlite3.Database(file);
 
 // Restify auth
 server.use(function(req, res, next) {
-	if (req.query.admin && req.query.admin == auth.adminName) {
-		return next();
+	if (req.query.a) {
+		if (req.query.a == auth.adminPass) {
+			return next();
+		} else {
+			console.log("invalid 'a' value provided");
+			res.send(404);
+			return next();
+		}
+	} else if (req.query.admin) {
+		let options = {json: true};
+		request(config.urlBase + req.query.admin, options, (error, res2, body) => {
+			if (error) { console.log(error); res.send(404); return next(); }
+			else if (!error && res.statusCode == 200) {
+				if (body && body['result'] == 1) {
+					console.log("success", req.connection.remoteAddress);
+					return next();
+				} else {
+					console.log("no body or result != 1", req.connection.remoteAddress);
+					res.send(404);
+					return next(false);
+				}
+			}
+		});
 	} else {
+		console.log("missing admin", req.connection.remoteAddress);
 		res.send(404);
 		return next(false);
 	}
@@ -47,7 +80,7 @@ server.get('/report', generateReport);
 
 // API functions
 function findLastName(req, res, next) {
-	var sql = "SELECT ID, FullName, FirstName, LastName, WTitle FROM latest_data WHERE LastName = '" + req.params.lastname + "'";
+	var sql = "SELECT ID, FullName, FirstName, LastName, WTitle, PMPNumber, PMPDate, PMIJoinDate, PMIExpirationDate, PMIAutoRenewStatus, ChapterAutoRenewStatus, DataDate FROM latest_data WHERE LastName = '" + req.params.lastname + "'";
 	db.all(sql, function(err, rows) {
 		if (err) {
 			res.send({errors: [err], meta: [], data: []});
